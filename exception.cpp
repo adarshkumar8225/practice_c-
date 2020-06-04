fe9f81890dfdf058e07689e131257261d97af1cc
adarshkumar8225 <51961214+adarshkumar8225@users.noreply.github.com>
2020-06-04 10:33:46 +0530

    Update exception.cpp

--- exception.cpp
+++ exception.cpp
@@ -20,7 +20,7 @@ bool checkUsername(string username) {
 	bool isValid = true;
 	int n = username.length();
 	if(n < 5) {
-		throw BadLengthException(n);
+		throw BadLengthException(n);//throw a exception.
 	}
 	for(int i = 0; i < n-1; i++) {
 		if(username[i] == 'w' && username[i+1] == 'w') {
@@ -35,14 +35,14 @@ int main() {
 	while(T--) {
 		string username;
 		cin >> username;
-		try {
+		try {//block where exception may occur.
 			bool isValid = checkUsername(username);
 			if(isValid) {
 				cout << "Valid" << '\n';
 			} else {
 				cout << "Invalid" << '\n';
 			}
-		} catch (BadLengthException e) {
+		} catch (BadLengthException e) { // execute this code when exception arises.
 			cout << "Too short: " << e.what() << '\n';
 		}
 	}
