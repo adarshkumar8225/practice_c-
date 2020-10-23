0ed737cebd956f2106404a8cfd679713e07fd584
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2020-10-23 16:29:04 +0530

    check whether input belong to list of keyword

---
+++ keyword.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+#include<math.h>
+using namespace std;
+string str1[]={"break","case","continue","default","defer","else","for","func","goto","if","map","range","return","struct","type","var"};
+
+int keyword(string str)
+{
+    int j;
+    for(j=0;j<16;j++)
+    {
+        if(str1[j].compare(str)==0)
+        {
+            return 1;
+        }
+    }
+    return 0;
+
+}
+int main()
+{
+    string str;
+    cin>>str;
+    int p=keyword(str);
+    if(p==0) cout<<str<<"  not a keyword"<<endl;
+    else cout<<str<<"  is a keyword";
+}
+
