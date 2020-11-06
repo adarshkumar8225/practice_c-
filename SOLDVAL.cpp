d655179a2d85de5c1a051bdb83620a831a213b54
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2020-11-06 11:03:02 +0530

    updated for optimized code

--- SOLDVAL.cpp
+++ SOLDVAL.cpp
@@ -6,34 +6,30 @@ int main()
 {
     int t;
     cin>>t;
-    while(t--)
+       while(t--)
     {
-        long int N;
-        cin>>N;
-        int A[N],F[N],i,j;
-        for(i=0;i<N;i++)
-        {
-            cin>>A[i];
-        }
+        int n;
+     
+        cin>>n;   
+        vector<int> a(n),l(n);
         
-            int min_v;
-            for(i=0,j=0;i<N;j++)
-            {
-                
-                
-                if(i>j) F[j]=A[j]+(i-j);
-                else F[j]=A[j]+(j-i);
-                if(j==0) min_v=F[j];
-                else if(min_v>F[j]) min_v=F[j];
-                if(j==N-1)
-                {
-                    i++;
-                    j=0;
-                    cout<<min_v<<" ";
-                    min_v=F[j]+abs(0-i);
-                }
-            }
+        for(int i=0;i<n;i++)
+        {    cin>>a[i];l[i]=a[i];}
+         
 
-        cout<<endl;
+        vector<int> r=l;
+            
+        //starting from i=1 find min of l[i] and (l[i-1]+1) store it in l[i]
+        for(int i=1;i<n;i++)
+            l[i]=min(l[i],l[i-1]+1);
+         //first make r=l then for r[i] take min of r[i] and r[i+1]+1 and store it in r[i].   
+        for(int i=n-2;i>=0;i--)
+            r[i]=min(r[i],r[i+1]+1);
+        //print min of the two for every index
+        for(int i=0;i<n;i++)
+            cout<<min(l[i],r[i])<<" ";
+            
+        cout<<"\n";
     }
 }
+
