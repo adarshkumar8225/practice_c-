fea7fc811b93a306d6af5c988f9bdb13d4fe050a
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2020-11-16 11:32:38 +0530

    recursion is used

--- UNSQUERS.cpp
+++ UNSQUERS.cpp
@@ -5,34 +5,42 @@
 using namespace std;
 
 vector<int> height;
-
+//vector<int > value;
+//************use recursion to calculate the result*****************************
 
 int calculate(int L,int R)
 {
-    int i,count,j,max,max_count;
-    if(L==R) return 1;
-    for(i=L;i<R;i++)
+    int i,temp,j,max_count;
+    static int max;
+    if(L==R)
     {
-        count=1;
-        max=height[i];
-        for(j=i+1;j<=R;j++)
+        
+        max=height[R];
+        
+        return 1;
+    }
+    else
+    {
+       /*// value.push_back(calculate(L+1,R));
+       // temp=value[value.size()-1];
+        temp=calculate(L+1,R);
+        if(height[L]<min) 
         {
-            if(height[j]>max) 
-            {
-                max=height[j];
-                count++;
-            }
-        }
-        if(count==(R-L+1)) return count;
-        else if(i==L) max_count=count;
-        else
-        {
-            if(count>max_count) max_count=count;
+            temp++;
+            min=height[L];
         }
         
+        return temp;*/
+        
+        temp=calculate(L,R-1);
+        if(height[R]>max) 
+        {
+            temp++;
+            max=height[R];
+        }
+        return temp;
     }
-    
-    return max_count;
+
 }
 
 
@@ -43,7 +51,7 @@ int main()
     int N,q,s;
     cin>>N>>q>>s;
     
-    int i,last=0,x,y,L,R,heights;
+    int i,last=0,x,y,L,R,heights,val,k;
     height.push_back(0);
     //store N heights to global vector can be accessed by all functn.
     for(i=0;i<N;i++)
@@ -64,10 +72,22 @@ int main()
             R^=L;
             L^=R;
         }
-        last=calculate(L,R);
+
+       last=calculate(L,R);
+       if(last==R-L+1){
+           cout<<last<<endl;
+           continue;
+       } 
+       for(k=L+1;k<=R;k++)
+       {
+            val=calculate(k,R);
+            if(val>last) last=val;
+
+       }
         cout<<last<<endl;
     }
 
 
 }
+
 
