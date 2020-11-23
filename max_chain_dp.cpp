760aa8eed25a2b942a9728a381de2bebc44b6b0c
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2020-11-23 16:54:08 +0530

    not completed yet

---
+++ max_chain_dp.cpp
@@ -0,0 +1,13 @@
+
+//array to store the value which is calculated.......
+int value[100];
+memset(value,0,sizeof(value));//initialize the array with 0
+
+int maxChainLen(struct val p[],int n)
+{
+    //check if for n it is already calculated or not<<<<<<<<<<<
+    if(value[n]!=0) return value[n];
+    
+    
+  
+}
