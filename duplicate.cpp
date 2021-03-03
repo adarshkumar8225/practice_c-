52e2c4391c3121cee5f2a4f96f5289e252ea33d1
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2021-03-03 19:49:00 +0530

    find duplicate number in the array

--- duplicate.cpp
+++ duplicate.cpp
@@ -1,11 +1,13 @@
 
 
-    int findDuplicate(vector<int>& nums) {
-        
+   int findDuplicate(vector<int>& nums)
+    {  
         vector<int> v;
-        for(int i=0;i<v.size();i++)
+        v.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++)
         {
-            if(v.find(nums[i])!=v.end()) return nums[i];
+            if(find(v.begin(),v.end(),nums[i])!=v.end()) return nums[i];
+            else v.push_back(nums[i]);
         }
-        
+        return 1;
     }
