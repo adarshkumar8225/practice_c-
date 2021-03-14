9d46e66ec0af00d4ba7b2e20c16a7ec2f848a8ff
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2021-03-14 19:40:07 +0530

    Equal partition sum O(N*Sum) DP

--- equal_subset.cpp
+++ equal_subset.cpp
@@ -1,35 +1,43 @@
 
 // Problem Link--->>> https://practice.geeksforgeeks.org/problems/subset-sum-problem2014/1#
 
-    int equalPartition(int N, int arr[])
+    
+    int equalPartition(int n, int arr[])
     {
-        // code here
-        int sum=0;
-        for(int i=0;i<N;i++) sum+=arr[i];
-        if(sum%2==1) return 0;
-        sum=sum/2;
-        int dp[N+1][sum+1];
-        if(N==1) return 0;
-        for(int i=0;i<=N;i++)
+    int sum = 0;
+    int i, j;
+ 
+    // Calculate sum of all elements
+    for (i = 0; i < n; i++)
+        sum += arr[i];
+ 
+    if (sum % 2 != 0)
+        return false;
+ 
+    bool part[n+1][sum / 2 + 1];
+    // Fill the partition table in bottom up manner
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= sum/2; j++) 
         {
-            for(int j=0;j<=sum;j++)
+            if(i==0)
             {
-                if(i==0 || j==0) 
+                if(j==0) part[i][j]=1;
+                else part[i][j]=0;
+            }
+            else if(j==0) part[i][j]=1;
+            else
+            {
+                //here we can either take current element or we can not take.
+                //Since boolean value so for either or " OR" operator is used.
+                if(arr[i-1]<=j) 
                 {
-                    dp[i][j]=0;
+                    part[i][j]=part[i-1][j]|| part[i-1][j-arr[i-1]];
                 }
-                
-                else
-                {
-                    if(arr[i-1]>j) dp[i][j]=dp[i-1][j];
-                    else 
-                    {
-                        if(j==arr[i-1]) dp[i][j]=1;
-                        else if(dp[i-1][j-arr[i-1]]==1) dp[i][j]=1;
-                        else dp[i][j]=dp[i-1][j];
-                    }
-                }
+                //if element is bigger than the sum=j so arr[i-1] cant be included. 
+                else part[i][j]=part[i-1][j];
             }
         }
-        return dp[N][sum];
     }
+
+    return part[n][sum / 2];
+}
