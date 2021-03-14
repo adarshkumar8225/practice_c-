a1e8d6e7ec00735a3becc3a48026e9a1c25d1289
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2021-03-14 16:56:21 +0530

    equal partition sum DP O(N*Sum of Element)

--- equal_subset.cpp
+++ equal_subset.cpp
@@ -1,21 +1,6 @@
 
 // Problem Link--->>> https://practice.geeksforgeeks.org/problems/subset-sum-problem2014/1#
 
-    bool subsequence(int arr[],int i,int N,int sum,int count)
-    {
-        if(sum==count) return 1;
-        if(i==N)
-        {
-            return 0;
-        }
-        
-        bool v=subsequence(arr,i+1,N,sum,count+arr[i]);
-        bool v1=subsequence(arr,i+1,N,sum,count);
-        
-        return(v||v1);
-        
-    }
-
     int equalPartition(int N, int arr[])
     {
         // code here
@@ -23,7 +8,28 @@
         for(int i=0;i<N;i++) sum+=arr[i];
         if(sum%2==1) return 0;
         sum=sum/2;
-        bool v=subsequence(arr,0,N,sum,0);
-        if(v) return 1;
-        else return 0;
+        int dp[N+1][sum+1];
+        if(N==1) return 0;
+        for(int i=0;i<=N;i++)
+        {
+            for(int j=0;j<=sum;j++)
+            {
+                if(i==0 || j==0) 
+                {
+                    dp[i][j]=0;
+                }
+                
+                else
+                {
+                    if(arr[i-1]>j) dp[i][j]=dp[i-1][j];
+                    else 
+                    {
+                        if(j==arr[i-1]) dp[i][j]=1;
+                        else if(dp[i-1][j-arr[i-1]]==1) dp[i][j]=1;
+                        else dp[i][j]=dp[i-1][j];
+                    }
+                }
+            }
+        }
+        return dp[N][sum];
     }
