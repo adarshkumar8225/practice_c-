04b480c95a1a3b00459d57cccb39ffbf416d6d21
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2021-03-26 11:18:10 +0530

    Max path sum in matrix O(N^2)

---
+++ max_path_matrix.cpp
@@ -0,0 +1,48 @@
+
+//Problem Statement-->>> https://practice.geeksforgeeks.org/problems/path-in-matrix3805/1#
+// Find maximum sum path in the matrix starting from any column of row 0.
+int max_m(int a,int b,int c)
+    {
+         int max;
+ 
+          if (a>b && a>c) max = a;
+          else if (b>c) max = b;
+          else max = c;
+        return max;
+    }
+    int maximumPath(int N, vector<vector<int>> Matrix)
+    {
+        // code here
+        int dp[N][N];
+        //Initialize the 2-D array with 1.
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<N;j++)
+            {
+                dp[i][j]=Matrix[i][j];
+            }
+        }
+        int max1=1;
+        for(int i=1;i<N;i++)
+        {
+            for(int j=0;j<N;j++)
+            {
+                if(j==0)
+                {
+                    dp[i][j]=max(dp[i][j]+dp[i-1][j],dp[i][j]+dp[i-1][j+1]);
+                    if(dp[i][j]>max1) max1=dp[i][j];
+                }
+                else if(j==N-1)
+                {
+                    dp[i][j]=max(dp[i][j]+dp[i-1][j],dp[i][j]+dp[i-1][j-1]);
+                    if(dp[i][j]>max1) max1=dp[i][j];
+                }
+                else
+                {
+                   dp[i][j]=max_m(dp[i][j]+dp[i-1][j],dp[i][j]+dp[i-1][j-1],dp[i][j]+dp[i-1][j+1]);
+                    if(dp[i][j]>max1) max1=dp[i][j];
+                }
+            }
+        }
+        return max1;
+    }
