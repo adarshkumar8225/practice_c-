5b90a2aaf436a74b045847fda6cab0503cfb09fb
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2021-03-30 17:44:04 +0530

    triplet sum in array O(N^2)

--- triplet.cpp
+++ triplet.cpp
@@ -1,31 +1,22 @@
 
-//Problem Link--->> 
+//Problem Link--->> https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1#
 // For given array find any triplet subset whose sum is equal to X, if yes return 1 else return 0..
-//Algorithm : Generate all subset with size 3 and find its sum , if it is equal to X return 1
 
-bool subset(int A[],vector<int> v,int X,int l,int n,bool val)
-    {
-        if(l==n)
-        {
-            if(v.size()==3)
-            {
-                if(X==accumulate(v.begin(),v.end(),0)) return 1;
-                else return 0;
-            }
-            return 0;
-        }
-        bool val1=subset(A,v,X,l+1,n,val);
-        v.push_back(A[l]);
-        bool val2=subset(A,v,X,l+1,n,val);
-        val= val1 || val2;
-        return val;
-    }
-    
-    
     bool find3Numbers(int A[], int n, int X)
     {
         //Your Code Here
-        vector<int> v;
-        bool val=0;
-        return (subset(A,v,X,0,n,val));
+        sort(A,A+n);
+        
+        int sum=0;
+        for(int i=0;i<n-2;i++)
+        {
+            for(int j=i+1,k=n-1;j<k;)
+            {
+                sum=A[i]+A[j]+A[k];
+                if(sum==X) return 1;
+                else if(sum<X) j++;
+                else k--;
+            }
+        }
+        return 0;
     }
