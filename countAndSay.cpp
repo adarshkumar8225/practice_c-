a832a33d6aa3eef45d167c80f574f5c3ad5054d8
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2021-04-08 16:28:31 +0530

    Recusive Solution to countAndSay problem

--- countAndSay.cpp
+++ countAndSay.cpp
@@ -1,16 +1,15 @@
 //Problem Link--> https://leetcode.com/problems/count-and-say/submissions/
 
 
-
     string countAndSay(int n) 
     {
-        string str1="1";
-        n=n-1;
-        while(n--)
-        {
-            string str="";
-            char c=str1[0];
-            int count=0;
+        string s="1";
+        if(n==1) return s;// when n==1 return string s="1"
+        string str1=countAndSay(n-1);//Recursive Call return the string
+        //Now count the number of different digits
+         string str="";
+         char c=str1[0];
+         int count=0;
            for(int i=0;i<str1.size();i++) 
            {
                if(c==str1[i]) count++;
@@ -22,9 +21,7 @@
                    i--;
                }
            }
-            str=str+to_string(count)+c;
-            str1=str;
-        }
-        return str1;
+         str=str+to_string(count)+c;
+        return str;
+        
     }
-
