ac38285acd89efd6e0b15f404c88d25eb05ba17b
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2021-05-21 08:50:20 +0530

    Optimized code using DP( Count Subset)

--- count_subset.cpp
+++ count_subset.cpp
@@ -1,37 +1,55 @@
 // Find no. of subset with sum=x ........................................
 
-// Recursive Approach --> Time Complexity= O(2^N)............................
+// Dynamic Programming Approach-->> Time Complexity= O(N * X)
+// N=number of elements , X=sum .
+
 
 #include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
-//Recursive function to count subset with sum equal to x.
-int subset_sum(int A[],int i,int n,int x)
+int count_subset(int A[],int N,int X)
 {
-    static int count;
-    // Base Condition..................
-    if(i==n) 
+    int dp[N+1][X+1];
+    for(int i=0;i<=N;i++)
     {
-        if(x==0) count=count+1;
-        return count;
+        for(int j=0;j<=X;j++)
+        {
+            //Initialization of first row and first column 
+            if(i==0)
+            {
+                if(j==0) dp[i][j]=1;
+                else dp[i][j]=0;
+            }
+            else if(j==0) dp[i][j]=1;
+            
+            else
+            {
+                //if current value A[i-1] is greater than required sum j 
+                //then current element cant be included.......
+                if(j<A[i-1]) dp[i][j]=dp[i-1][j];
+                // Otherwise take the sum (Not include current element + Include current element)
+                else
+                dp[i][j]=dp[i-1][j]+dp[i-1][j-A[i-1]];
+            }
+        }
     }
+    //return the value in last cell of 2-D array
+    return dp[N][X];
     
-    else
-    {
-        subset_sum(A,i+1,n,x);
-        subset_sum(A,i+1,n,x-A[i]);
-    }
 }
+
 
 int main()
 {
-    int n;
-    cin>>n;
-    int A[n];
-    for(int i=0;i<n;i++) cin>>A[i];
-    int x;
-    cin>>x;
-    cout<<subset_sum(A,0,n,x);
+    int N;
+    cin>>N;
+    int A[N];
+    for(int i=0;i<N;i++) cin>> A[i];
+    int X;
+    cin>>X;
+    
+    cout<<count_subset(A,N,X);
+
     return 0;
 }
