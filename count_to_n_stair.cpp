dc402eaee3016c0ede491427e33b6210ebdbda95
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2021-09-22 21:00:00 +0530

    count ways to reach the Nth Stair

--- count_to_n_stair.cpp
+++ count_to_n_stair.cpp
@@ -1,20 +1,25 @@
-    int count=0;
-    int DP[10000];
+    
+     int DP[10000];
+//Recursive function to return the result............
     int count_ways(int i,int n)
     {
         if(i==n) 
         {
-            //count+=1;
+            
             return 1;
         }
         else if(i>n) return 0;
-        return count_ways(i+1,n)+
-        count_ways(i+2,n);
+        if(DP[i]!=-1) return DP[i];
+        //we can take step of 1 stair or 2 stair.............
+        DP[i]=count_ways(i+1,n)%1000000007+
+        count_ways(i+2,n)%1000000007;
+        return DP[i];
     }
     
     
     //Function to count number of ways to reach the nth stair.
     int countWays(int n)
     {
+        for(int i=0;i<10000;i++) DP[i]=-1;
         return count_ways(0,n)%1000000007;
     }
